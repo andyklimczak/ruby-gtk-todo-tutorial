51cb41efdbd2eeef35105fafec263274fd813836
Andy Klimczak <andyklimczak@fastmail.com>
2018-02-09 23:14:02 -0500

    define model

--- application/ui/todo/application.rb
+++ application/ui/todo/application.rb
@@ -4,10 +4,10 @@ module ToDo
       super 'com.andy.gtk-todo', Gio::ApplicationFlags::FLAGS_NONE
 
       signal_connect :activate do |application|
-        window = Gtk::ApplicationWindow.new(application)
-        window.set_title 'Hello gtk+ruby and'
+        window = Todo::ApplicationWindow.new(application)
         window.present
       end
     end
   end
 end
+
---
+++ application/ui/todo/application_window.rb
@@ -0,0 +1,20 @@
+module Todo
+  class ApplicationWindow < Gtk::ApplicationWindow
+    # Register the class in the GLib world
+    type_register
+
+    class << self
+      def init
+        # Set the template from the resources binary
+        set_template resource: '/com/andy/gtk-todo/ui/application_window.ui'
+      end
+    end
+
+    def initialize(application)
+      super application: application
+
+      set_title 'GTK+ Simple ToDo'
+    end
+  end
+end
+
